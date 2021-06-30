0f5a31226e9354bff93d9873a1a6a0073fccc0e4
ForzBox7 <salmananaqvii+github@gmail.com>
2021-06-30 14:24:28 +0500

    Implemented a basic bubble sort.

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,7 +1,15 @@
-# Keep looping till stopping index.
-# Compare the first and second numbers.
-# If the first number is larger than the second, swap them.
-# Compare the second and third numbers.
-# If the second number is larger than the third, swap them.
-# Continue till the the stopping index.
-# Keep repeating this till the stopping index is equal to the first index.
+my_list = [25, 34, 98, 7, 41, 19, 5]
+stopping_index = len(my_list) - 1
+
+# Outer loop so that we go through each element in the list.
+for x in range(len(my_list)):
+    for current_index in range(stopping_index):
+        if my_list[current_index] > my_list[current_index+1]:
+            # Swap values.
+            temp = my_list[current_index]
+            my_list[current_index] = my_list[current_index+1]
+            my_list[current_index+1] = temp
+    # So that we don't compare the already sorted values.
+    stopping_index -= 1
+
+print(my_list)
