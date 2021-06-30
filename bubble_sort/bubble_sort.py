a95df0db3a7eca0d0898c142febfb641abee4135
ForzBox7 <salmananaqvii+github@gmail.com>
2021-06-30 14:29:53 +0500

    Refactored the algorithm in a function.

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,15 +1,13 @@
-my_list = [25, 34, 98, 7, 41, 19, 5]
-stopping_index = len(my_list) - 1
-
-# Outer loop so that we go through each element in the list.
-for x in range(len(my_list)):
-    for current_index in range(stopping_index):
-        if my_list[current_index] > my_list[current_index+1]:
-            # Swap values.
-            temp = my_list[current_index]
-            my_list[current_index] = my_list[current_index+1]
-            my_list[current_index+1] = temp
-    # So that we don't compare the already sorted values.
-    stopping_index -= 1
-
-print(my_list)
+def bubble_sort(list):
+    """Does a bubble sort in ascending order on the provided list."""
+    stopping_index = len(list) - 1
+    # Outer loop so that we go through each element in the list.
+    for x in range(len(list)):
+        for current_index in range(stopping_index):
+            if list[current_index] > list[current_index + 1]:
+                # Swap values.
+                temp = list[current_index]
+                list[current_index] = list[current_index + 1]
+                list[current_index + 1] = temp
+        # So that we don't compare the already sorted values.
+        stopping_index -= 1
