d343c951425a12e81e480282616cf171c7cf6039
ForzBox7 <salmananaqvii+github@gmail.com>
2021-06-30 12:28:35 +0500

    Pseudocoded my own implementation.

---
+++ bubble_sort/bubble_sort.py
@@ -0,0 +1,7 @@
+# Keep looping till stopping index.
+# Compare the first and second numbers.
+# If the first number is larger than the second, swap them.
+# Compare the second and third numbers.
+# If the second number is larger than the third, swap them.
+# Continue till the the stopping index.
+# Keep repeating this till the stopping index is equal to the first index.
