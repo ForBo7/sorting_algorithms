1981563d9b22cd529f806707bb37c31915573f9f
ForzBox7 <salmananaqvii+github@gmail.com>
2021-07-04 17:18:22 +0500

    Created unit tests for the insertion sort algorthm.

---
+++ insertion_sort/test_insertion_sort.py
@@ -0,0 +1,23 @@
+import unittest
+from insertion_sort import insertion_sort
+
+class InsertionSortTestCase(unittest.TestCase):
+    """Tests for insertion sort."""
+
+    def test_unordered_list(self):
+        """Test if the insertion sort works in an unordered list."""
+        list = [25, 34, 98, 7, 41, 19, 5]
+        insertion_sort(list)
+        self.assertEqual([5, 7, 19, 25, 34, 41, 98], list)
+
+    def test_semiordered_list(self):
+        """Test if the algorithm works in a semiordered list."""
+        list = [43, 21, 19, 59, 62, 79]
+        insertion_sort(list)
+        self.assertEqual([19, 21, 43, 59, 62, 79], list)
+
+    def test_single_element_list(self):
+        """Test if the insertion sort works in a list with a single element."""
+        list = [5]
+        insertion_sort(list)
+        self.assertEqual([5], list)
